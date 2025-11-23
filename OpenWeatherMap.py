6308a7d89245ba928ea4d392f4e943ba521f1f9f
BKaiHAW <gornodon4@gmx.de>
2025-11-23 15:33:17 +0100

    Koordinaten für Standardstädte

--- OpenWeatherMap.py
+++ OpenWeatherMap.py
@@ -8,8 +8,12 @@ from datetime import datetime
 API_KEY = "76d3c2c705805e009533b5cee35fafef"
 
 cities = {
-    "test": (1, 2),
-    "hamburg": (53.550341, 10.000654)
+    "hamburg": (53.550341, 10.000654),
+    "hamburg": (53.5511, 9.9937),
+    "berlin": (52.5200, 13.4050),
+    "frankfurt": (50.1109, 8.6821),
+    "muenchen": (48.1351, 11.5820),
+    "koeln": (50.9375, 6.9603)
 }
 
 def get_city_coords(city_name: str) -> tuple[float, float]:
